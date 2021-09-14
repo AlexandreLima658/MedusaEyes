1341e26cc474c4480cefa2d61df68fce561e08a9
AlexandreLima658 <alexandrec@alu.ufc.br>
2021-09-14 10:57:46 -0300

    Arrow function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
-function Home() {
-  return (
-    <div className='home'>
+export default () => 
+   <div className='home'>
       <h1>Home</h1>
     </div>
-  )
-}
+  
 
-export default Home
+
+
